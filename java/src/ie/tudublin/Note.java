1f5d38b655a744feabe4715f331163b34d634a1e
angesh021 <angesh021@gmail.com>
2021-04-21 16:21:04 +0100

    Note Class implemented

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,39 @@
+package ie.tudublin;
+
+public class Note 
+{
+    private String note;
+    private int duration;
+
+    //constructor method
+    public Note(String note, int duration)
+    {
+        this.note = note;
+        this.duration = duration;
+    }
+
+    //Getter and setter
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    //to string method
+    @Override
+    public String toString() {
+        return "Note [duration=" + duration + ", note=" + note + "]";
+    }
+    
+    
+}
